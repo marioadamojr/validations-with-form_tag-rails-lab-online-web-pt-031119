63307b643cc1ee885de4f10d6efea32134c7d017
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-10 17:58:34 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -17,6 +17,16 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(post_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+      # render :"/authors/edit"
+    end
+  end
+
   private
 
   def author_params
