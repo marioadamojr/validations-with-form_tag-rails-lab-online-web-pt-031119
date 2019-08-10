bce1a2e4149706dabf67f6c66ad6bbb662655be6
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-10 17:44:25 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,8 @@ class PostsController < ApplicationController
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
-      render :"/posts/edit"
+      render :edit
+      # render :"/posts/edit"
     end
   end
 
