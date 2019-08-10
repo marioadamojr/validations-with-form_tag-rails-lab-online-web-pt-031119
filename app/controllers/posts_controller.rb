2c695527e4a77be5074e4bf7444cdc33fe1e3ec7
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-10 17:59:19 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
